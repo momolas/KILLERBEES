72f613c4211776b9fc44ff297cab5ae53000b606
Momo L'As <momolas@users.noreply.github.com>
2023-04-23 17:05:09 +0200

    Initial Commit

---
+++ KILLERBEES/KILLERBEESApp.swift
@@ -0,0 +1,17 @@
+//
+//  KILLERBEESApp.swift
+//  KILLERBEES
+//
+//  Created by Mo on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct KILLERBEESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
