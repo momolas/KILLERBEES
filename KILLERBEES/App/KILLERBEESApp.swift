2f737246023381e50146b99cb7e274f3c44c0e00
Momo L'As <momolas@users.noreply.github.com>
2026-09-04 19:17:56 +0200

    feat: disable idle timer to keep screen awake while app is active

--- KILLERBEES/App/KILLERBEESApp.swift
+++ KILLERBEES/App/KILLERBEESApp.swift
@@ -5,12 +5,14 @@
 //  Created by Mo on 23/04/2023.
 //
 
-import SwiftUI
-import GroundSdk
 import ArsdkEngine
+import GroundSdk
+import SwiftUI
+import UIKit
 
 @main
 struct KILLERBEESApp: App {
+    @SwiftUI.Environment(\.scenePhase) private var scenePhase: ScenePhase
     @State private var droneManager: DroneManager
 
     init() {
@@ -22,6 +24,19 @@ struct KILLERBEESApp: App {
         WindowGroup {
             ContentView()
                 .environment(droneManager)
+                .onAppear {
+                    UIApplication.shared.isIdleTimerDisabled = true
+                }
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            switch newPhase {
+            case .active:
+                UIApplication.shared.isIdleTimerDisabled = true
+            case .inactive, .background:
+                UIApplication.shared.isIdleTimerDisabled = false
+            @unknown default:
+                break
+            }
         }
     }
 }
